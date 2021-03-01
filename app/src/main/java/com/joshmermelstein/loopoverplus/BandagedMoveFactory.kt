0023477c4a71e16803b52c1a79dd19d851959724
Josh Mermeltein <joshmermel@gmail.com>
2021-03-01 18:42:31 -0500

    fix bug where col move could cause infinite loop.

--- app/src/main/java/com/joshmermelstein/loopoverplus/BandagedMoveFactory.kt
+++ app/src/main/java/com/joshmermelstein/loopoverplus/BandagedMoveFactory.kt
@@ -1,5 +1,7 @@
 package com.joshmermelstein.loopoverplus
 
+import android.util.Log
+
 // Returns wide moves according to the positions of bonds
 class BandagedMoveFactory : MoveFactory {
     override fun makeMove(
@@ -24,20 +26,26 @@ class BandagedMoveFactory : MoveFactory {
             while (board.rowContainsBond(retOffset, Bond.UP) && depth <= board.numCols) {
                 retOffset -= 1
                 depth += 1
+                Log.d("jmerm", "$retOffset , $depth")
             }
             while (board.rowContainsBond(retOffset + depth - 1, Bond.DOWN) && depth <= board.numCols
             ) {
                 depth += 1
+                Log.d("jmerm", "$retOffset , $depth")
             }
         } else {
             while (board.colContainsBond(retOffset, Bond.LEFT) && depth <= board.numRows) {
                 retOffset -= 1
                 depth += 1
+                Log.d("jmerm", "$retOffset , $depth")
+
             }
-            while (board.colContainsBond(retOffset + depth - 1, Bond.RIGHT)) {
+            while (board.colContainsBond(retOffset + depth - 1, Bond.RIGHT) && depth <= board.numRows) {
                 depth += 1
+                Log.d("jmerm", "$retOffset , $depth")
             }
         }
+        Log.d("jmerm", "done")
         return Pair(retOffset, depth)
     }
 
