03fd06c05fed7d52c37e4e86f439ad807712d596
Josh Mermeltein <joshmermel@gmail.com>
2021-04-02 21:36:16 -0400

    add test for static cells move factory

---
+++ app/src/androidTest/java/com/joshmermelstein/loopoverplus/StaticCellsMoveFactoryTest.kt
@@ -0,0 +1,65 @@
+package com.joshmermelstein.loopoverplus
+
+import androidx.test.platform.app.InstrumentationRegistry
+import junit.framework.TestCase
+
+class StaticCellsMoveFactoryTest : TestCase() {
+    private val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+    private val numRows = 4
+    private val numCols = 3
+    private val arr = arrayOf(
+        "1", "2", "3",
+        "4", "5", "6",
+        "7", "8", "9",
+        "10", "11", "F 12"
+    )
+    private val board = GameBoard(numRows, numCols, arr, appContext)
+
+    fun testMakeMoveHorizontal() {
+        val factory = StaticCellsMoveFactory(2,1)
+        val move = factory.makeMove(Axis.HORIZONTAL, Direction.BACKWARD, 1, board)
+        val expected = WideMove(Axis.HORIZONTAL, Direction.BACKWARD, 1, numRows, numCols, 2)
+        assertEquals(move, expected)
+
+        val illegalMove = factory.makeMove(Axis.HORIZONTAL, Direction.BACKWARD, 2, board)
+        val expectedIllegal = IllegalMove(listOf(Pair(3,2)))
+        assertEquals(illegalMove, expectedIllegal)
+    }
+
+    fun testMakeMoveVertical() {
+        val factory = StaticCellsMoveFactory(3,2)
+        val move = factory.makeMove(Axis.VERTICAL, Direction.FORWARD, 0, board)
+        val expected = WideMove(Axis.VERTICAL, Direction.FORWARD, 0, numRows, numCols, 2)
+        assertEquals(move, expected)
+
+        val illegalMove = factory.makeMove(Axis.VERTICAL, Direction.FORWARD, 1, board)
+        val expectedIllegal = IllegalMove(listOf(Pair(3,2)))
+        assertEquals(illegalMove, expectedIllegal)
+    }
+
+    fun testFindLockedCells() {
+        val arr = arrayOf("F 1", "F 2", "F 3", "F 4")
+        val board = GameBoard(2, 2, arr, appContext)
+        val factory = StaticCellsMoveFactory(2,2)
+        val move = factory.makeMove(Axis.VERTICAL, Direction.FORWARD, 0, board)
+        val expected = IllegalMove(listOf(Pair(0,0),Pair(1,0),Pair(0,1),Pair(1,1)))
+        assertEquals(move, expected)
+    }
+
+    fun testMakeHighlightsHorizontal() {
+        val factory = StaticCellsMoveFactory(2,3)
+        val highlights = factory.makeHighlights(Axis.HORIZONTAL, Direction.BACKWARD, 1, board)
+        assertEquals(highlights.size, 2)
+        assertEquals(highlights[0], Highlight(Axis.HORIZONTAL, Direction.BACKWARD, 1))
+        assertEquals(highlights[1], Highlight(Axis.HORIZONTAL, Direction.BACKWARD, 2))
+    }
+
+    fun testMakeHighlightsVertical() {
+        val factory = StaticCellsMoveFactory(2,3)
+        val highlights = factory.makeHighlights(Axis.VERTICAL, Direction.FORWARD, 1, board)
+        assertEquals(highlights.size, 3)
+        assertEquals(highlights[0], Highlight(Axis.VERTICAL, Direction.FORWARD, 1))
+        assertEquals(highlights[1], Highlight(Axis.VERTICAL, Direction.FORWARD, 2))
+        assertEquals(highlights[2], Highlight(Axis.VERTICAL, Direction.FORWARD, 3))
+    }
+}
