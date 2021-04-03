602eaf34213dd36013d5601ec8cd2fda33953895
Josh Mermeltein <joshmermel@gmail.com>
2021-04-03 14:38:25 -0400

    add test for illegal moves

---
+++ app/src/androidTest/java/com/joshmermelstein/loopoverplus/IllegalMoveTest.kt
@@ -0,0 +1,41 @@
+package com.joshmermelstein.loopoverplus
+
+import androidx.test.platform.app.InstrumentationRegistry
+import junit.framework.TestCase
+
+class IllegalMoveTest : TestCase() {
+    private val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+    private val numRows = 2
+    private val numCols = 2
+    private val arr = arrayOf("F 1", "E", "F 2", "E")
+    private val board = GameBoard(numRows, numCols, arr, appContext)
+    private val move = IllegalMove(listOf(Pair(0,0), Pair(0,1)))
+
+    fun testTestRunTooSoon() {
+        move.run(board, 50, 100, 0)
+        // Too early, none fo the cells are modified
+        assertFalse(board.getCell(0,0).shouldDrawIcon)
+        assertFalse(board.getCell(0,1).shouldDrawIcon)
+        assertFalse(board.getCell(1,0).shouldDrawIcon)
+        assertFalse(board.getCell(1,1).shouldDrawIcon)
+    }
+
+    fun testTestRunTooLate() {
+        // Move is complete, all cells return to normal
+        move.run(board, 50, 100, 150)
+        assertFalse(board.getCell(0,0).shouldDrawIcon)
+        assertFalse(board.getCell(0,1).shouldDrawIcon)
+        assertFalse(board.getCell(1,0).shouldDrawIcon)
+        assertFalse(board.getCell(1,1).shouldDrawIcon)
+    }
+
+    fun testTestRunNormally() {
+        // In the middle, listed cells are modified
+        move.run(board, 50, 100, 75)
+        assertTrue(board.getCell(0,0).shouldDrawIcon)
+        assertTrue(board.getCell(0,1).shouldDrawIcon)
+
+        assertFalse(board.getCell(1,0).shouldDrawIcon)
+        assertFalse(board.getCell(1,1).shouldDrawIcon)
+    }
+}
