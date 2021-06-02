7a68f2e7a9636e5a7af0b7051b45a09f603d7009
Josh Mermeltein <joshmermel@gmail.com>
2021-06-01 22:35:04 -0400

    update menu logic to allow row modes without col modes.

--- app/src/main/java/com/joshmermelstein/loopoverplus/InfinityActivity.kt
+++ app/src/main/java/com/joshmermelstein/loopoverplus/InfinityActivity.kt
@@ -14,62 +14,58 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.parcelize.Parcelize
 import kotlin.random.Random
 
-// TODO(jmerm): support for additional modes
+// TODO(jmerm): move board generating logic to own file
+// TODO(jmerm): add spinner for num enablers, num_bandaged, etc
 
 @Parcelize
 class RandomLevelParams(
     val numRows: Int,
     val numCols: Int,
     val rowFactory: String,
-    val colFactory: String,
+    val colFactory: String?,
     val rowDepth: Int?,
     val colDepth: Int?
 ) : Parcelable
 
 // Activity for letting the user build a level
-class InfinityActivity : AppCompatActivity() {
+class InfinityActivity : AppCompatActivity(),  AdapterView.OnItemSelectedListener {
     private val rowSizes = (2..6).map { num -> num.toString() }
     private val colSizes = (2..4).map { num -> num.toString() }
-    private val modes = arrayOf("Wide", "Carousel", "Gear")
+    private val rowModes = arrayOf("Wide", "Carousel", "Gear", "Dynamic Bandaging", "Static Cells", "Enabler")
+    private val colModes = arrayOf("Wide", "Carousel", "Gear")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_infinity)
         configureSizePickers()
-        configureModePickers()
+        configureRowModePicker()
         configureButton()
     }
 
-    // TODO(jmerm): randomize these?
     private fun configureSizePickers() {
         val rowSizeSpinner = findViewById<Spinner>(R.id.rowSizeSpinner)
         val rowAdapter = ArrayAdapter(this, R.layout.spinner_item, rowSizes)
         rowAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
         rowSizeSpinner.adapter = rowAdapter
-
-        rowSizeSpinner.onItemSelectedListener = SelectionMadeListener(::updateRowDepthPicker)
+        rowSizeSpinner.onItemSelectedListener = this
+        rowSizeSpinner.setSelection(Random.nextInt(1, 3))
 
         val colSizeSpinner = findViewById<Spinner>(R.id.colSizeSpinner)
         val colAdapter = ArrayAdapter(this, R.layout.spinner_item, colSizes)
         colAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
         colSizeSpinner.adapter = colAdapter
-        colSizeSpinner.onItemSelectedListener = SelectionMadeListener(::updateColDepthPicker)
+        colSizeSpinner.onItemSelectedListener = this
+        colSizeSpinner.setSelection(Random.nextInt(1, 3))
+
     }
 
-    // TODO(jmerm): randomize these?
-    private fun configureModePickers() {
+    private fun configureRowModePicker() {
         val rowModeSpinner = findViewById<Spinner>(R.id.rowModeSpinner)
-        val rowAdapter = ArrayAdapter(this, R.layout.spinner_item, modes)
+        val rowAdapter = ArrayAdapter(this, R.layout.spinner_item, rowModes)
         rowAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
         rowModeSpinner.adapter = rowAdapter
-        rowModeSpinner.onItemSelectedListener = SelectionMadeListener(::updateRowDepthPicker)
-
-
-        val colModeSpinner = findViewById<Spinner>(R.id.colModeSpinner)
-        val colAdapter = ArrayAdapter(this, R.layout.spinner_item, modes)
-        colAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
-        colModeSpinner.adapter = colAdapter
-        colModeSpinner.onItemSelectedListener = SelectionMadeListener(::updateColDepthPicker)
+        rowModeSpinner.onItemSelectedListener = this
+        rowModeSpinner.setSelection(Random.nextInt(0, rowModes.size + 1) % rowModes.size)
     }
 
     private fun getNumRows(): Int {
@@ -84,8 +80,8 @@ class InfinityActivity : AppCompatActivity() {
         return findViewById<Spinner>(R.id.rowModeSpinner).selectedItem.toString()
     }
 
-    private fun getColMode(): String {
-        return findViewById<Spinner>(R.id.colModeSpinner).selectedItem.toString()
+    private fun getColMode(): String? {
+        return findViewById<Spinner>(R.id.colModeSpinner).selectedItem?.toString()
     }
 
     private fun getRowDepth(): Int? {
@@ -96,10 +92,30 @@ class InfinityActivity : AppCompatActivity() {
         return findViewById<Spinner>(R.id.colDepthSpinner).selectedItem?.toString()?.toInt()
     }
 
+    private fun updateColModePicker() {
+        val container = findViewById<View>(R.id.col_mode_container)
+        if (colModes.contains(getRowMode())) {
+            if (container?.visibility == View.VISIBLE) {
+                return
+            }
+            container.visibility = View.VISIBLE
+            val colModeSpinner = findViewById<Spinner>(R.id.colModeSpinner)
+            val colAdapter = ArrayAdapter(this, R.layout.spinner_item, colModes)
+            colAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
+            colModeSpinner.adapter = colAdapter
+            colModeSpinner.onItemSelectedListener = this
+            colModeSpinner.setSelection(Random.nextInt(0, colModes.size + 1) % colModes.size)
+        } else if (container?.visibility == View.VISIBLE) {
+            container?.visibility = View.GONE
+            findViewById<Spinner>(R.id.colModeSpinner).adapter = null
+        }
+    }
+
     private fun updateRowDepthPicker() {
-        if (getRowMode() == "Wide") {
+        val container = findViewById<View>(R.id.row_depth_container)
+        if (getRowMode() == "Wide" || getRowMode() == "Static Cells") {
             val oldDepth = getRowDepth()
-            findViewById<View>(R.id.row_depth_container)?.visibility = View.VISIBLE
+            container?.visibility = View.VISIBLE
             val rowDepthSpinner = findViewById<Spinner>(R.id.rowDepthSpinner)
             val options = (1..getNumRows()).map { num -> num.toString() }
             val adapter = ArrayAdapter(this, R.layout.spinner_item, options)
@@ -113,14 +129,15 @@ class InfinityActivity : AppCompatActivity() {
                 }
             }
         } else {
-            findViewById<View>(R.id.row_depth_container)?.visibility = View.GONE
+            container?.visibility = View.GONE
         }
     }
 
     private fun updateColDepthPicker() {
-        if (getColMode() == "Wide") {
+        val container = findViewById<View>(R.id.col_depth_container)
+        if (getColMode() == "Wide" || getRowMode() == "Static Cells") {
             val oldDepth = getColDepth()
-            findViewById<View>(R.id.col_depth_container)?.visibility = View.VISIBLE
+            container?.visibility = View.VISIBLE
             val colDepthSpinner = findViewById<Spinner>(R.id.colDepthSpinner)
             val options = (1..getNumCols()).map { num -> num.toString() }
             val adapter = ArrayAdapter(this, R.layout.spinner_item, options)
@@ -135,8 +152,15 @@ class InfinityActivity : AppCompatActivity() {
                 }
             }
         } else {
-            findViewById<View>(R.id.col_depth_container)?.visibility = View.GONE
+            container?.visibility = View.GONE
         }
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>?) {}
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        updateColModePicker()
+        updateRowDepthPicker()
+        updateColDepthPicker()
     }
 
     private fun configureButton() {
@@ -160,22 +184,15 @@ class InfinityActivity : AppCompatActivity() {
     }
 }
 
-// Helper class for calling a void callback when the user picks an option.
-class SelectionMadeListener(val action: () -> Unit) : AdapterView.OnItemSelectedListener {
-    override fun onNothingSelected(parent: AdapterView<*>?) {}
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) =
-        action()
-}
-
 fun fromRandomFactory(name: String, rowDepth: Int?, colDepth: Int?): MoveFactory {
-    return if (name == "Gear") {
-        GearMoveFactory()
-    } else if (name == "Carousel") {
-        CarouselMoveFactory()
-    } else if (name == "Wide") {
-        WideMoveFactory(rowDepth!!, colDepth!!)
-    } else {
-        BasicMoveFactory()
+    return when (name) {
+        "Gear" -> GearMoveFactory()
+        "Carousel" -> CarouselMoveFactory()
+        "Wide" -> WideMoveFactory(rowDepth!!, colDepth!!)
+        "Enabler" -> EnablerMoveFactory()
+        "Dynamic Bandaging" -> DynamicBandagingMoveFactory()
+        "Static Cells" -> StaticCellsMoveFactory(rowDepth!!, colDepth!!)
+        else -> BasicMoveFactory()
     }
 }
 
@@ -183,8 +200,7 @@ fun randomMove(
     board: GameBoard,
     factory: MoveFactory,
     num_rows: Int,
-    num_cols: Int,
-    context: Context
+    num_cols: Int
 ): Move {
     val direction = if (Random.nextBoolean()) {
         Direction.FORWARD
@@ -199,12 +215,13 @@ fun randomMove(
     }
 }
 
+// TODO(jmerm): make scramble logic work in S, D, E modes
 fun scramble(
     solved: Array<String>, factory: MoveFactory, num_rows: Int, num_cols: Int, context: Context
 ): Array<String> {
     val gameBoard = GameBoard(num_rows, num_cols, solved, context)
     for (i in (0..1000)) {
-        val move = randomMove(gameBoard, factory, num_rows, num_cols, context)
+        val move = randomMove(gameBoard, factory, num_rows, num_cols)
         move.finalize(gameBoard)
     }
     return gameBoard.toString().split(",").toTypedArray()
@@ -214,17 +231,19 @@ fun scramble(
 fun generateRandomLevel(options: RandomLevelParams, context: Context): GameplayParams {
     // make factory
     val factory: MoveFactory =
-        if (options.rowFactory == options.colFactory) {
+        if (options.rowFactory == options.colFactory || options.colFactory == null) {
             fromRandomFactory(options.rowFactory, options.rowDepth, options.colDepth)
         } else {
             CombinedMoveFactory(
                 fromRandomFactory(options.rowFactory, options.rowDepth, options.colDepth),
-                fromRandomFactory(options.colFactory, options.rowDepth, options.colDepth)
+                fromRandomFactory(options.colFactory!!, options.rowDepth, options.colDepth)
             )
         }
 
+    // TODO(jmerm): add in E, and F cells as necessary
     val goal = (0..23).filter { (it < options.numRows * 4) && (it % 4 < options.numCols) }
         .map { i -> (i + 1).toString() }.toTypedArray()
+    // TODO(jmerm): handle case where start == goal?
     val start = scramble(goal, factory, options.numRows, options.numCols, context)
 
     return GameplayParams(
