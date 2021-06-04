54fbd47def4d0835164d5c005a4fe328d2ff5abc
Josh Mermeltein <joshmermel@gmail.com>
2021-06-04 18:02:56 -0400

    better way to do spinner updates to avoid recursive spinner updates.

--- app/src/main/java/com/joshmermelstein/loopoverplus/InfinityActivity.kt
+++ app/src/main/java/com/joshmermelstein/loopoverplus/InfinityActivity.kt
@@ -14,13 +14,12 @@ import kotlin.random.Random
 
 // TODO(jmerm): add spinner for num enablers, num_bandaged, etc
 // TODO(jmerm): allow more rows and columns when the colorscheme is bicolor
-// TODO(jmerm): limit row/col depth more in static mode than wide mode
 
 @Parcelize
 class RandomLevelParams(
     val numRows: Int,
     val numCols: Int,
-    val colorScheme : String,
+    val colorScheme: String,
     val rowMode: String,
     val colMode: String?,
     val rowDepth: Int?,
@@ -28,10 +27,11 @@ class RandomLevelParams(
 ) : Parcelable
 
 // Activity for letting the user build a level
-class InfinityActivity : AppCompatActivity(),  AdapterView.OnItemSelectedListener {
+class InfinityActivity : AppCompatActivity() {
     private val rowSizes = (2..6).map { num -> num.toString() }
     private val colSizes = (2..4).map { num -> num.toString() }
-    private val rowModes = arrayOf("Wide", "Carousel", "Gear", "Dynamic Bandaging", "Static Cells", "Enabler")
+    private val rowModes =
+        arrayOf("Wide", "Carousel", "Gear", "Dynamic Bandaging", "Static Cells", "Enabler")
     private val colModes = arrayOf("Wide", "Carousel", "Gear")
     private val colorSchemes = arrayOf("Bicolor", "Columns", "Unique")
 
@@ -49,14 +49,15 @@ class InfinityActivity : AppCompatActivity(),  AdapterView.OnItemSelectedListene
         val rowAdapter = ArrayAdapter(this, R.layout.spinner_item, rowSizes)
         rowAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
         rowSizeSpinner.adapter = rowAdapter
-        rowSizeSpinner.onItemSelectedListener = this
+        rowSizeSpinner.onItemSelectedListener = SelectionMadeListener(::onUpdateNumRows)
+
         rowSizeSpinner.setSelection(Random.nextInt(1, 3))
 
         val colSizeSpinner = findViewById<Spinner>(R.id.colSizeSpinner)
         val adapter = ArrayAdapter(this, R.layout.spinner_item, colSizes)
         adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
         colSizeSpinner.adapter = adapter
-        colSizeSpinner.onItemSelectedListener = this
+        colSizeSpinner.onItemSelectedListener = SelectionMadeListener(::onUpdateNumCols)
         colSizeSpinner.setSelection(Random.nextInt(1, 3))
 
     }
@@ -66,7 +67,7 @@ class InfinityActivity : AppCompatActivity(),  AdapterView.OnItemSelectedListene
         val adapter = ArrayAdapter(this, R.layout.spinner_item, rowModes)
         adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
         rowModeSpinner.adapter = adapter
-        rowModeSpinner.onItemSelectedListener = this
+        rowModeSpinner.onItemSelectedListener = SelectionMadeListener(::onUpdateRowMode)
         rowModeSpinner.setSelection(Random.nextInt(0, rowModes.size + 1) % rowModes.size)
 
     }
@@ -77,7 +78,7 @@ class InfinityActivity : AppCompatActivity(),  AdapterView.OnItemSelectedListene
 
         adapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
         colorSchemeSpinner.adapter = adapter
-        colorSchemeSpinner.onItemSelectedListener = this
+        colorSchemeSpinner.onItemSelectedListener = SelectionMadeListener(::onUpdateColorScheme)
         // colorSchemeSpinner.setSelection(Random.nextInt(0, rowModes.size + 1) % rowModes.size)
     }
 
@@ -120,7 +121,7 @@ class InfinityActivity : AppCompatActivity(),  AdapterView.OnItemSelectedListene
             val colAdapter = ArrayAdapter(this, R.layout.spinner_item, colModes)
             colAdapter.setDropDownViewResource(R.layout.spinner_dropdown_item)
             colModeSpinner.adapter = colAdapter
-            colModeSpinner.onItemSelectedListener = this
+            colModeSpinner.onItemSelectedListener = SelectionMadeListener(::onUpdateColMode)
             colModeSpinner.setSelection(Random.nextInt(0, colModes.size + 1) % colModes.size)
         } else if (container?.visibility == View.VISIBLE) {
             container?.visibility = View.GONE
@@ -173,11 +174,25 @@ class InfinityActivity : AppCompatActivity(),  AdapterView.OnItemSelectedListene
         }
     }
 
-    override fun onNothingSelected(parent: AdapterView<*>?) {}
-    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+    private fun onUpdateColorScheme() {
+        // TODO(jmerm)
+    }
+
+    private fun onUpdateRowMode() {
         updateColModePicker()
         updateRowDepthPicker()
         updateColDepthPicker()
+    }
+    private fun onUpdateColMode() {
+        updateColDepthPicker()
+    }
+
+    private fun onUpdateNumCols() {
+        updateColDepthPicker()
+    }
+
+    private fun onUpdateNumRows() {
+        updateRowDepthPicker()
     }
 
     private fun configureButton() {
@@ -200,4 +215,11 @@ class InfinityActivity : AppCompatActivity(),  AdapterView.OnItemSelectedListene
             getColDepth()
         )
     }
+}
+
+// Helper class for calling a void callback when the user picks an option.
+class SelectionMadeListener(val action: () -> Unit) : AdapterView.OnItemSelectedListener {
+    override fun onNothingSelected(parent: AdapterView<*>?) {}
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) =
+        action()
 }
