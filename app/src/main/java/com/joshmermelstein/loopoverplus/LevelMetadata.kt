7cec4b35bb803b83ff1cdbd14a90a3e508c0c6dc
Josh Mermeltein <joshmermel@gmail.com>
2021-06-12 21:48:52 -0400

    minor formatting

--- app/src/main/java/com/joshmermelstein/loopoverplus/LevelMetadata.kt
+++ app/src/main/java/com/joshmermelstein/loopoverplus/LevelMetadata.kt
@@ -41,7 +41,6 @@ class MetadataSingleton private constructor(context: Context) {
             "static",
             "hybrid_wc",
             "hybrid_wg",
-            // "hybrid_cg",
         )) {
             val reader =
                 BufferedReader(InputStreamReader(context.assets.open("packs/$filename.txt")))
