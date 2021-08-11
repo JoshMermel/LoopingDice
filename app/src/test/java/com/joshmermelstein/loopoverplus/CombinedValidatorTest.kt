013e0490287fd098c17a7559716a283d3d07526e
Josh Mermeltein <joshmermel@gmail.com>
2021-08-10 21:36:01 -0400

    silly tests for triple-intersections of validators

--- app/src/test/java/com/joshmermelstein/loopoverplus/CombinedValidatorTest.kt
+++ app/src/test/java/com/joshmermelstein/loopoverplus/CombinedValidatorTest.kt
@@ -242,5 +242,111 @@ class CombinedValidatorTest : TestCase() {
         )
     }
 
-    // TODO(jmerm): triple validation hybrids? (A+E+S, A+E+D)
+    fun testArrowsEnablerStatic() {
+        val factory = MoveFactory(
+            BasicMoveEffect(Axis.HORIZONTAL),
+            BasicMoveEffect(Axis.VERTICAL),
+            CombinedValidator(listOf(ArrowsValidator(), EnablerValidator(), StaticCellsValidator()))
+        )
+
+        val numRows = 4
+        val numCols = 3
+        val arr = arrayOf(
+            "F 0", "H 1", "2",
+            "V E 3", "4", "V E 5",
+            "6", "7", "8",
+            "9", "H E 10", "11"
+        )
+        val board = GameBoard(numRows, numCols, arr, fakeGameCellMetadata())
+
+        // Legal row
+        assertEquals(
+            factory.makeMove(Axis.HORIZONTAL, Direction.FORWARD, 3, board),
+            BasicMove(Axis.HORIZONTAL, Direction.FORWARD, 3, numRows, numCols)
+        )
+        // Illegal row
+        assertEquals(
+            factory.makeMove(Axis.HORIZONTAL, Direction.BACKWARD, 0, board),
+            IllegalMove(
+                lockCords = listOf(Pair(0, 0)),
+                keyCords = listOf(Pair(1, 0), Pair(1, 2), Pair(3, 1))
+            )
+        )
+        assertEquals(
+            factory.makeMove(Axis.HORIZONTAL, Direction.BACKWARD, 1, board),
+            IllegalMove(lockCords = listOf(Pair(1, 0), Pair(1,2)))
+        )
+        assertEquals(
+            factory.makeMove(Axis.HORIZONTAL, Direction.BACKWARD, 2, board),
+            IllegalMove(keyCords = listOf(Pair(1, 0), Pair(1, 2), Pair(3, 1)))
+        )
+        // Legal col
+        assertEquals(
+            factory.makeMove(Axis.VERTICAL, Direction.FORWARD, 2, board),
+            BasicMove(Axis.VERTICAL, Direction.FORWARD, 2, numRows, numCols)
+        )
+        // Illegal col
+        assertEquals(
+            factory.makeMove(Axis.VERTICAL, Direction.BACKWARD, 0, board),
+            IllegalMove(lockCords = listOf(Pair(0, 0)))
+        )
+        assertEquals(
+            factory.makeMove(Axis.VERTICAL, Direction.BACKWARD, 1, board),
+            IllegalMove(lockCords = listOf(Pair(0, 1), Pair(3,1)))
+        )
+    }
+
+    fun testArrowsDynamicEnabler() {
+        val factory = MoveFactory(
+            BasicMoveEffect(Axis.HORIZONTAL),
+            BasicMoveEffect(Axis.VERTICAL),
+            CombinedValidator(listOf(ArrowsValidator(), DynamicBandagingValidator(), EnablerValidator()))
+        )
+
+        val numRows = 4
+        val numCols = 3
+        val arr = arrayOf(
+            "F V 0", "1", "2",
+            "E 3", "F 4", "H 5",
+            "6", "7", "8",
+            "V E 9", "10", "E 11"
+        )
+        val board = GameBoard(numRows, numCols, arr, fakeGameCellMetadata())
+
+        // Legal row
+        assertEquals(
+            factory.makeMove(Axis.HORIZONTAL, Direction.FORWARD, 1, board),
+            BasicMove(Axis.HORIZONTAL, Direction.FORWARD, 1, numRows, numCols)
+        )
+        // Illegal row
+        assertEquals(
+            factory.makeMove(Axis.HORIZONTAL, Direction.BACKWARD, 0, board),
+            IllegalMove(
+                lockCords = listOf(Pair(0, 0)),
+                keyCords = listOf(Pair(1, 0), Pair(3, 0), Pair(3, 2))
+            )
+        )
+        assertEquals(
+            factory.makeMove(Axis.HORIZONTAL, Direction.BACKWARD, 2, board),
+            IllegalMove(keyCords = listOf(Pair(1, 0), Pair(3, 0), Pair(3, 2)))
+        )
+        assertEquals(
+            factory.makeMove(Axis.HORIZONTAL, Direction.BACKWARD, 3, board),
+            IllegalMove(lockCords = listOf(Pair(3, 0)))
+        )
+        // Legal col
+        assertEquals(
+            factory.makeMove(Axis.VERTICAL, Direction.FORWARD, 0, board),
+            BasicMove(Axis.VERTICAL, Direction.FORWARD, 0, numRows, numCols)
+        )
+        // Illegal col
+        assertEquals(
+            factory.makeMove(Axis.VERTICAL, Direction.BACKWARD, 0, board),
+            IllegalMove(lockCords = listOf(Pair(0, 0)))
+        )
+        assertEquals(
+            factory.makeMove(Axis.VERTICAL, Direction.BACKWARD, 2, board),
+            IllegalMove(lockCords = listOf(Pair(1, 2)))
+        )
+    }
 }
